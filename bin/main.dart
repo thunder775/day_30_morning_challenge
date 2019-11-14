d05578e3076c5e6323d123d834b34dbb29f6410a
McLarenCollege <hello@mclarencollege.in>
2019-11-14 08:30:07 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,17 @@
+// Challenge
+// Matryoshka dolls are traditionally wooden dolls that can be nested by fitting
+// smaller dolls into larger ones. Suppose arrays can be nested similarly,
+// placing smaller arrays into larger ones, in the following sense:
+//
+//// Array A can be nested inside Array B if:
+//// 1. min(array A) > min(array B)
+//// 2. max(array A) < max(array B)
+
+//// For example, if A = [2, 3, 9, 5] and B = [10, 2, 1], then A can be nested inside B, since:
+////  - min(A) = 2 > 1 = min(B) and
+////   - max(A) = 9 < 10 = max(B)
+
+/// Create a function that returns true if every single sub-array inside an array
+/// can be nested Matroyshka style, and false otherwise.
+main() {
+}
